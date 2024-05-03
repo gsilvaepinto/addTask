4948441cd24a838d04b21b23b51757730479de97
Guilherme Pinto <109026846+gsilvaepinto@users.noreply.github.com>
2024-05-03 03:41:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 let input = prompt('What would you like to do?');
-const task = [];
+const tasks = [];
 
-while (input !== 'quit' && input !== 'q'){
-    if (input == 'list'){
-        console.log('********');
-        for (let i = 0; i < task.length; i++){
-            console.log(`${i}: ${task[i]}`.toUpperCase());
+while (input !== 'q' && input !== 'quit'){
+    if (input === 'list'){
+        console.log('******');
+        for(let i = 0; i < tasks.length; i++){
+            console.log(`${i}: ${tasks[i].toUpperCase()}`);
         }
-        console.log('********');
-    } else if (input == 'new') {
-        const addTask = prompt("Enter new task:");
-        task.push(addTask);
-        console.log(`${addTask.toUpperCase()} added to your list.`)
-    } else if (input == 'delete'){
-        const index = parseInt(prompt("Select index to delete:"));
-        if (!Number.isNaN(index) && index >= 0 && index < task.length){
-            const deleted = task.splice(index, 1);
-            console.log(`${deleted} was deleted.`);
+        console.log('******');
+    } else if (input === 'new'){
+        const addTask = prompt('Enter new task:');
+        tasks.push(addTask);
+        console.log(`${addTask.toUpperCase()} added to your list.`) 
+    } else if (input === 'delete'){
+        const index = parseInt(prompt('Select index to delete:'));
+        if (!Number.isNaN(index) && index >= 0 && index < tasks.length){
+            const deleted = tasks.splice(index, 1);
+            console.log(`${deleted[0].toUpperCase()} deleted from your list.`);
         } else {
-            console.log(`Index does not exist. Please try again.`);
+            console.log('Invalid index. Please try again.');
         }
+    } else {
+        console.log('Invalid command. Please try again.');
     }
     input = prompt('What would you like to do?');
 }
